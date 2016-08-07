6be33828490f6c129ee33f9476c8e8837c8886ec
Lucy Xiao <lucyxiao@berkely.edu>
2016-08-06 22:31:40 -0700

    Added comments, deleted unnecessary components, overall cleanup

--- ImageDisplayGUI/src/imagedisplaygui/ImageDisplayGUI.java
+++ ImageDisplayGUI/src/imagedisplaygui/ImageDisplayGUI.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package imagedisplaygui;
 
 import java.awt.GraphicsDevice;
@@ -16,8 +11,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 import java.util.ArrayList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -27,22 +20,15 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 import java.security.cert.X509Certificate;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.concurrent.WorkerStateEvent;
-import javafx.event.EventType;
 import javafx.scene.CacheHint;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -52,18 +38,15 @@ import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.stage.Modality;
 
 /**
  *
  * @author lucyxiao
  */
 public class ImageDisplayGUI extends Application {
-//        private GridPane grid = new GridPane();
+        //spacing is the space between two images
         private int spacing = 5;
+        private int numImgPerRow = 5;
         private int screenWidth;
         private int screenHeight;
         ObservableList<String> entries = FXCollections.observableArrayList();    
@@ -71,176 +54,84 @@ public class ImageDisplayGUI extends Application {
         GridPane grid = new GridPane();
         GridPane grid2 = new GridPane();
         ScrollPane sp = new ScrollPane();
-
-        
+ 
     @Override
     public void start(Stage primaryStage) throws NoSuchAlgorithmException, KeyManagementException {
-        
+        //Initialize the main grid layout
         grid = initializeGridLayout(grid);
 
+        //Initializes user's screenWidth and screenHeight
         getScreenWidthHeight();
         Scene scene = new Scene(grid, screenWidth, screenHeight);
         scene.getStylesheets().add("imagedisplaygui/ImageDisplayCss/ImageDisplayGUI.css");
         
-        
+        //Some links have a certificate issue, this will bypass most of them
         fixHttpsIssue();
         
+        //Initialize Array of URL strings
         ArrayList<String> imgArr = getStrings();
                
-//        Text scenetitle =  new Text("Images");
-//        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-        primaryStage.setTitle("Simple Search");
-        
         // Set up the ListView
         list.setMaxHeight(100);
 
+        //Add all data to an observabe ArrayList
         for (String entry : imgArr) {
             entries.add(entry);
         }
+        //Might delete list which displays entries
         list.setItems( entries );
 
 //        grid2.setGridLinesVisible(true);
 //        grid.setGridLinesVisible(true);
-//        ScrollPane sp = new ScrollPane(grid2);
+
+        //Tells the ScrollPane to scroll only grid2
+        //grid2 is the grid for images
         sp.setContent(grid2);
         grid.add(sp, 0, 2);
         grid.setHgrow(sp, Priority.ALWAYS);
         
+        //Adds search form
         TextField txt = new TextField();
         txt.setPromptText("Search");
         txt.setPrefSize(screenWidth*0.6,20);
         txt.getStyleClass().add("form-control");
-        txt.textProperty().addListener(
-            new ChangeListener() {
-                public void changed(ObservableValue observable, 
-                                    Object oldVal, Object newVal) {
-                    handleSearchByKey2((String)oldVal, (String)newVal);
-//                    addImgToGrid();
-//                    sp.setContent(grid2);
-    
-                }
 
-            });
-        Label scenetitle = new Label("Twitter Image Search");
-        scenetitle.setFont(Font.font("Tamoha", FontWeight.NORMAL, 72));
-        
+        //Adds search button
         Button searchButton = new Button("Search");
         searchButton.setPrefSize(100, 20);
         searchButton.getStyleClass().add("button-search");
         searchButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override public void handle(ActionEvent e) {
-//                handleSearchByKey2("", txt.textProperty().toString());
+                updateList("", txt.getCharacters().toString());
                 addImgToGrid(primaryStage);
                 sp.setContent(grid2);
             }
         });
-//        grid.add(scenetitle, 0, 0, 2, 1);
-        HBox root = new HBox();
-        root.getChildren().addAll(txt,searchButton);
         
-//        root.setPadding(new Insets(10,10,10,10));
-//        root.setSpacing(2);
+        //Creates the container for search
+        HBox searchBox = new HBox();
+        searchBox.getChildren().addAll(txt,searchButton);    
+        grid.add(searchBox, 0, 1);  
+        searchBox.setAlignment(Pos.CENTER);
         
-        grid.add(root, 0, 1);  
-        root.setAlignment(Pos.CENTER);
+        //Creates page title label
+        Label scenetitle = new Label("Twitter Image Search");
+        scenetitle.setFont(Font.font("Tamoha", FontWeight.NORMAL, 72));
         StackPane p = new StackPane();
         p.setPrefSize(700,100);
         p.getChildren().add(scenetitle);
         StackPane.setAlignment(scenetitle,Pos.CENTER);
         grid.add(p, 0,0);
-//        double centerTitle  = (scenetitle.widthProperty().doubleValue()/2.0);
-//        System.out.println(scenetitle.widthProperty().doubleValue());
-//        root.setMargin(scenetitle, new Insets(10,centerTitle,10,centerTitle));
-        
-        
+
+        //Sets up page
         primaryStage.setTitle("San Fransokyo");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
     
-    public void addImgToGrid(Stage primaryStage) {
-        grid2 = new GridPane();
-        grid2.setVgap(spacing);
-        int hbRow = 1;
-        int hbWidth = 0;
-        HBox hb = new HBox(spacing);
-
-        double perfectImgWidth;
-        int count =0 ;
-        perfectImgWidth = (screenWidth - spacing * 4.0 - 17 - 20)/5.0;
-            for (Object imgUrlObj : list.getItems()) {
-               String imgUrl = (String) imgUrlObj;
-               Image img = new Image(imgUrl, perfectImgWidth, 0, true, false, true);
-               ImageView imgView = new ImageView();
-               imgView.setCache(true);
-               imgView.setCacheHint(CacheHint.SPEED);
-               imgView.setImage(img);
-                
-               imgView.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
-
-                    @Override
-                    public void handle(MouseEvent event) {
-                         final Clipboard clipboard = Clipboard.getSystemClipboard();
-                        final ClipboardContent content = new ClipboardContent();
-                        content.putString(imgUrl);
-                        content.putHtml("<b>"+imgUrl+"</b>");
-                        clipboard.setContent(content);  
-                        
-                        Label label = new Label("Copied URL to Clipboard");
-                        label.setFont(Font.font("Tamoha", FontWeight.NORMAL, 36));
-                        StackPane centeredLabel = new StackPane(label);
-                        centeredLabel.setStyle("-fx-background-color: white; -fx-opacity: 0.8;");
-
-                        StackPane stack = new StackPane(centeredLabel);
-                        stack.setPrefSize(100, 50);
-                        stack.setPadding(new Insets(10));
-
-//                        stack.getChildren().addAll(clipboardTxt,r);
-                        grid.add(stack, 0, 2);
-                        stack.setAlignment(Pos.CENTER);
-                         Task<Void> sleeper = new Task<Void>() {
-                            @Override
-                            protected Void call() throws Exception {
-                                try {
-                                    
-                                    
-                                    Thread.sleep(1000);
-                                } catch (InterruptedException e) {
-                                }
-                                return null;
-                            }
-                        };
-                        sleeper.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
-                            @Override
-                            public void handle(WorkerStateEvent event) {
-                                grid.getChildren().remove(stack);
-                            }
-                        });
-                        try {
-                            new Thread(sleeper).start();  
-                        } catch (Exception e) {
-                            grid.getChildren().remove(stack);
-                        }
-    
-                    }
-               });
-               
-                hbWidth += perfectImgWidth;
-                count += 1;
-                System.out.println(count + ": " + (hbWidth ) + " vs " + (screenWidth - spacing * 4.0 - 17));
-               if (hbWidth >= screenWidth - spacing * 4.0 - 17) {             
-                    grid2.add(hb, 0, hbRow);
-                    hb = new HBox(spacing);
-                    hbRow += 1;
-                    hbWidth = (int) perfectImgWidth;
-               }
-               hb.getChildren().add(imgView);
-            }
-        grid2.add(hb, 0, hbRow);
-        
-    }
- 
-    public void handleSearchByKey2(String oldVal, String newVal) {
+    //Called everytime Search is clicked
+    //Updates list of images relevant to search term
+    public void updateList(String oldVal, String newVal) {
         // If the number of characters in the text box is less than last time
         // it must be because the user pressed delete
         if ( oldVal != null && (newVal.length() < oldVal.length()) ) {
@@ -255,7 +146,7 @@ public class ImageDisplayGUI extends Application {
  
         // Filter out the entries that don't contain the entered text
         ObservableList<String> subentries = FXCollections.observableArrayList();
-        for ( Object entry: list.getItems() ) {
+        for ( Object entry: entries) {
             boolean match = true;
             String entryText = (String)entry;
             for ( String part: parts ) {
@@ -266,12 +157,106 @@ public class ImageDisplayGUI extends Application {
                     break;
                 }
             }
- 
             if ( match ) {
                 subentries.add(entryText);
             }
         }
         list.setItems(subentries);
+    }
+    
+    //Updates image grid
+    public void addImgToGrid(Stage primaryStage) {
+        // Need to create a new grid every call/search
+        grid2 = new GridPane();
+        grid2.setVgap(spacing);
+        // hbRow : Grid Row Number
+        int hbRow = 1;
+        // hbWidth : HBox Width so far
+        int hbWidth = 0;
+        HBox hb = new HBox(spacing);
+
+        int count = 0;
+        
+        // Calculates width for images to fit numImgPerRow
+        // ScrollBar has width 17 pixels, grid2 has inset 10 pixel each side
+        // Double counted spacing for last image in row so we have to subtract spacing
+        double perfectImgWidth = (screenWidth - spacing * 4.0 - 17 - 20)/numImgPerRow;
+            
+        for (Object imgUrlObj : list.getItems()) {
+           String imgUrl = (String) imgUrlObj;
+           //Set settings to make images load faster
+           Image img = new Image(imgUrl, perfectImgWidth, 0, true, false, true);
+           ImageView imgView = new ImageView();
+           imgView.setCache(true);
+           imgView.setCacheHint(CacheHint.SPEED);
+           imgView.setImage(img);
+
+            // If image is clicked, copy URL to clipboard
+            imgView.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+
+                @Override
+                public void handle(MouseEvent event) {
+                    final Clipboard clipboard = Clipboard.getSystemClipboard();
+                    final ClipboardContent content = new ClipboardContent();
+                    content.putString(imgUrl);
+                    content.putHtml("<b>"+imgUrl+"</b>");
+                    clipboard.setContent(content);  
+
+                    //Displays a quick and temporary modal 
+                    Label label = new Label("Copied URL to Clipboard");
+                    label.setFont(Font.font("Tamoha", FontWeight.NORMAL, 36));
+                    StackPane centeredLabel = new StackPane(label);
+                    centeredLabel.setStyle("-fx-background-color: white; -fx-opacity: 0.8;");
+
+                    StackPane stack = new StackPane(centeredLabel);
+                    stack.setPrefSize(100, 50);
+                    stack.setPadding(new Insets(10));
+
+                    grid.add(stack, 0, 2);
+                    stack.setAlignment(Pos.CENTER);
+
+                    // Creates a sleeper task to close modal automatically
+                    Task<Void> sleeper = new Task<Void>() {
+                        @Override
+                        protected Void call() throws Exception {
+                            try { 
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                            }
+                            return null;
+                        }
+                    };
+                    sleeper.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+                        @Override
+                        public void handle(WorkerStateEvent event) {
+                            grid.getChildren().remove(stack);
+                        }
+                    });
+                    try {
+                        new Thread(sleeper).start();  
+                    } catch (Exception e) {
+                        grid.getChildren().remove(stack);
+                    }
+                }
+            });
+            
+           
+            hbWidth += perfectImgWidth;
+            count += 1;
+            System.out.println(count + ": " + (hbWidth ) + " vs " + (screenWidth - spacing * 4.0 - 17));
+            
+            // If row width is bigger than available space, add img to next row
+            if (hbWidth >= screenWidth - spacing * 4.0 - 17 - 20) {             
+                grid2.add(hb, 0, hbRow);
+                hb = new HBox(spacing);
+                hbRow += 1;
+                hbWidth = (int) perfectImgWidth;
+            }
+            hb.getChildren().add(imgView);
+        }
+        // Add last row
+        grid2.add(hb, 0, hbRow);
+        
     }
     
     /**
@@ -300,16 +285,16 @@ public class ImageDisplayGUI extends Application {
         
         return arr;
     }
+    
     private GridPane initializeGridLayout(GridPane grid) {
-//        grid.setAlignment(Pos.CENTER);
         grid.setHgap(20);
         grid.setVgap(20);
         grid.setPadding(new Insets(10,10,10,10));
         return grid;
     }
     
+    // Sets size of application to system screen size.
     private void getScreenWidthHeight() {
-       // Sets size of application to system screen size.
         GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
         screenWidth = gd.getDisplayMode().getWidth();
         screenHeight = gd.getDisplayMode().getHeight();
@@ -330,20 +315,9 @@ public class ImageDisplayGUI extends Application {
 
     /**
      * @param args the command line arguments
-     * @throws java.lang.ClassNotFoundException
-     * @throws java.lang.InstantiationException
-     * @throws java.lang.IllegalAccessException
-     * @throws javax.swing.UnsupportedLookAndFeelException
      */
-    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
-        if (System.getProperty("os.name").equals("Mac OS X")) {
-            System.out.println("Got in!");
-            System.setProperty("apple.laf.useScreenMenuBar", "true");
-            System.setProperty("com.apple.mrj.application.apple.menu.about.name", "San Fransokyo TITLE");
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        }
+    public static void main(String[] args) {
         Application.launch(args);
-        System.out.println(System.getProperty("os.name"));
     }
     
 }
