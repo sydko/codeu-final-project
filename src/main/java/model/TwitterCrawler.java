167a6798f9211a15a4b88d090f427e5074988f48
Thomas Lilly <tlilly3@gatech.edu>
2016-08-07 23:52:41 -0400

    changed to correct package

--- src/main/java/model/TwitterCrawler.java
+++ src/main/java/model/TwitterCrawler.java
@@ -1,4 +1,4 @@
-package com.flatironschool.javacs;
+package model;
 
 import java.io.IOException;
 import java.util.LinkedList;
--- src/main/java/model/TwitterFetcher.java
+++ src/main/java/model/TwitterFetcher.java
@@ -1,4 +1,4 @@
-package com.flatironschool.javacs;
+package model;
 
 import java.io.File;
 import java.io.IOException;
