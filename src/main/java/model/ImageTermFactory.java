c5dd2151611af7c40af71f73127a65ff5f0a1628
Thomas Lilly <tlilly3@gatech.edu>
2016-08-09 21:48:59 -0400

    cosmetic changes

--- src/main/java/model/ImageTermFactory.java
+++ src/main/java/model/ImageTermFactory.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2016 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package model;
 
 // [START import_libraries]
@@ -49,11 +33,8 @@ import java.util.HashMap;
  */
 @SuppressWarnings("serial")
 public class ImageTermFactory {
-    /**
-     * Be sure to specify the name of your application. If the application name is {@code null} or
-     * blank, the application will log a warning. Suggested format is "MyCompany-ProductName/1.0".
-     */
-    private static final String APPLICATION_NAME = "Google-VisionLabelSample/1.0";
+
+    private static final String APPLICATION_NAME = "codeu-final-project/1.0";
 
     private static final int MAX_LABELS = 300;
 
